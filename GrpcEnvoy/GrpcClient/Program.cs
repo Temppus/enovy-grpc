ebce5e4421e49590949dd256be2927a828b4873e
Ivan Benovic <ivan.benovic@innovatrics.com>
2025-12-27 19:50:01 +0100

    Create GprcClient and Server boilerplate projects

---
+++ GrpcEnvoy/GrpcClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+
+namespace GrpcClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:7244");
+            var client = new Greeter.GreeterClient(channel);
+            var request = new HelloRequest { Name = "GreeterClient" };
+            var reply = await client.SayHelloAsync(request);
+            Console.WriteLine("Received greeting response message: " + reply.Message);
+            Console.ReadKey();
+        }
+    }
+}
